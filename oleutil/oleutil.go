7e7827b5ee5660779ab668e7375e0879ac7e886c
mattn <mattn.jp@gmail.com>
2011-01-24 11:27:05 +0900

    Get IDispatch directly.

--- oleutil/oleutil.go
+++ oleutil/oleutil.go
@@ -2,30 +2,33 @@ package oleutil
 
 import "ole"
 import "os"
+import "unsafe"
 
 func CreateDispatch(progId string) (dispatch *ole.IDispatch, err os.Error) {
 	var clsid *ole.GUID
 	clsid, err = ole.CLSIDFromProgID(progId)
 	if err != nil {
-		return
+		clsid, err = ole.CLSIDFromString(progId)
+		if err != nil {
+			return
+		}
 	}
 
 	var unknown *ole.IUnknown
-	unknown, err = ole.CreateInstance(clsid)
+	unknown, err = ole.CreateInstance(clsid, ole.IID_IDispatch)
 	if err != nil {
 		return
 	}
-
-	dispatch, err = unknown.QueryInterface(ole.IID_IDispatch)
-	if err != nil {
-		return
-	}
+	dispatch = (*ole.IDispatch)(unsafe.Pointer(unknown))
 	return
 }
 
 func CallMethod(disp *ole.IDispatch, name string, params ...interface{}) (result *ole.VARIANT, err os.Error) {
 	var dispid []int32
 	dispid, err = disp.GetIDsOfName([]string{name})
+	if err != nil {
+		return
+	}
 	result, err = disp.Invoke(dispid[0], ole.DISPATCH_METHOD, params...)
 	return
 }
@@ -33,6 +36,9 @@ func CallMethod(disp *ole.IDispatch, name string, params ...interface{}) (result
 func GetProperty(disp *ole.IDispatch, name string, params ...interface{}) (result *ole.VARIANT, err os.Error) {
 	var dispid []int32
 	dispid, err = disp.GetIDsOfName([]string{name})
+	if err != nil {
+		return
+	}
 	result, err = disp.Invoke(dispid[0], ole.DISPATCH_PROPERTYGET, params...)
 	return
 }
@@ -40,6 +46,9 @@ func GetProperty(disp *ole.IDispatch, name string, params ...interface{}) (resul
 func PutProperty(disp *ole.IDispatch, name string, params ...interface{}) (result *ole.VARIANT, err os.Error) {
 	var dispid []int32
 	dispid, err = disp.GetIDsOfName([]string{name})
+	if err != nil {
+		return
+	}
 	result, err = disp.Invoke(dispid[0], ole.DISPATCH_PROPERTYPUT, params...)
 	return
 }
