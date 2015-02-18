e5e751faa5242f8312bafb48e0ba53fb447dcb35
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-02-18 11:22:31 +0900

    Add some type conversion for Value()

--- variant.go
+++ variant.go
@@ -47,10 +47,40 @@ func (v *VARIANT) Clear() error {
 // and will be returned as strings.
 func (v *VARIANT) Value() interface{} {
 	switch v.VT {
-	case VT_I2, VT_I4:
-		return v.Val
+	case VT_I1:
+		return int8(v.Val)
+	case VT_UI1:
+		return uint8(v.Val)
+	case VT_I2:
+		return int16(v.Val)
+	case VT_UI2:
+		return uint16(v.Val)
+	case VT_I4:
+		return int32(v.Val)
+	case VT_UINT:
+		return uint32(v.Val)
+	case VT_INT_PTR:
+		return uintptr(v.Val) // TODO
+	case VT_UINT_PTR:
+		return uintptr(v.Val)
+	case VT_UI4:
+		return uint32(v.Val)
+	case VT_I8:
+		return int64(v.Val)
+	case VT_UI8:
+		return uint64(v.Val)
+	case VT_R4:
+		return float32(v.Val)
+	case VT_R8:
+		return float64(v.Val)
 	case VT_BSTR:
 		return v.ToString()
+	//case VT_DATE:
+	//	return v.ToIDispatch() // TODO: use VariantTimeToSystemTime
+	case VT_UNKNOWN:
+		return v.ToIUnknown()
+	case VT_DISPATCH:
+		return v.ToIDispatch()
 	case VT_BOOL:
 		return v.Val != 0
 	}
