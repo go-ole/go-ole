f9cd260cb36809622762248df59ca908c9817177
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-02-18 14:11:14 +0900

    Fix example

--- example/msxml/rssreader.go
+++ example/msxml/rssreader.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/mattn/go-ole"
@@ -11,25 +12,31 @@ func main() {
 	ole.CoInitialize(0)
 	unknown, _ := oleutil.CreateObject("Microsoft.XMLHTTP")
 	xmlhttp, _ := unknown.QueryInterface(ole.IID_IDispatch)
-	oleutil.CallMethod(xmlhttp, "open", "GET", "http://rss.slashdot.org/Slashdot/slashdot", false)
-	oleutil.CallMethod(xmlhttp, "send", nil)
+	_, err := oleutil.CallMethod(xmlhttp, "open", "GET", "http://rss.slashdot.org/Slashdot/slashdot", false)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = oleutil.CallMethod(xmlhttp, "send", nil)
+	if err != nil {
+		panic(err.Error())
+	}
 	state := -1
 	for state != 4 {
 		state = int(oleutil.MustGetProperty(xmlhttp, "readyState").Val)
 		time.Sleep(10000000)
 	}
 	responseXml := oleutil.MustGetProperty(xmlhttp, "responseXml").ToIDispatch()
-	items := oleutil.MustCallMethod(responseXml, "selectNodes", "rdf:RDF/item").ToIDispatch()
+	items := oleutil.MustCallMethod(responseXml, "selectNodes", "/rss/channel/item").ToIDispatch()
 	length := int(oleutil.MustGetProperty(items, "length").Val)
 
 	for n := 0; n < length; n++ {
 		item := oleutil.MustGetProperty(items, "item", n).ToIDispatch()
 
 		title := oleutil.MustCallMethod(item, "selectSingleNode", "title").ToIDispatch()
-		println(oleutil.MustGetProperty(title, "text").ToString())
+		fmt.Println(oleutil.MustGetProperty(title, "text").ToString())
 
 		link := oleutil.MustCallMethod(item, "selectSingleNode", "link").ToIDispatch()
-		println("  " + oleutil.MustGetProperty(link, "text").ToString())
+		fmt.Println("  " + oleutil.MustGetProperty(link, "text").ToString())
 
 		title.Release()
 		link.Release()
