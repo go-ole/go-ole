43313d26faab1a82c5cdd084ea92c13c2f538f54
Jacob Santos <jacob@essential-elements.net>
2015-05-15 10:54:02 -0500

    Move windows syscall to its own file to prevent build errors.

--- oleutil/connection.go
+++ oleutil/connection.go
@@ -1,8 +1,9 @@
+// +build windows
+
 package oleutil
 
 import (
 	"reflect"
-	"syscall"
 	"unsafe"
 
 	ole "github.com/go-ole/go-ole"
@@ -24,52 +25,6 @@ type stdDispatchVtbl struct {
 	pGetTypeInfo      uintptr
 	pGetIDsOfNames    uintptr
 	pInvoke           uintptr
-}
-
-// ConnectObject creates a connection point between two services for communication.
-func ConnectObject(disp *ole.IDispatch, iid *ole.GUID, idisp interface{}) (cookie uint32, err error) {
-	unknown, err := disp.QueryInterface(ole.IID_IConnectionPointContainer)
-	if err != nil {
-		return
-	}
-
-	container := (*ole.IConnectionPointContainer)(unsafe.Pointer(unknown))
-	var point *ole.IConnectionPoint
-	err = container.FindConnectionPoint(iid, &point)
-	if err != nil {
-		return
-	}
-	if edisp, ok := idisp.(*ole.IUnknown); ok {
-		cookie, err = point.Advise(edisp)
-		container.Release()
-		if err != nil {
-			return
-		}
-	}
-	rv := reflect.ValueOf(disp).Elem()
-	if rv.Type().Kind() == reflect.Struct {
-		dest := &stdDispatch{}
-		dest.lpVtbl = &stdDispatchVtbl{}
-		dest.lpVtbl.pQueryInterface = syscall.NewCallback(dispQueryInterface)
-		dest.lpVtbl.pAddRef = syscall.NewCallback(dispAddRef)
-		dest.lpVtbl.pRelease = syscall.NewCallback(dispRelease)
-		dest.lpVtbl.pGetTypeInfoCount = syscall.NewCallback(dispGetTypeInfoCount)
-		dest.lpVtbl.pGetTypeInfo = syscall.NewCallback(dispGetTypeInfo)
-		dest.lpVtbl.pGetIDsOfNames = syscall.NewCallback(dispGetIDsOfNames)
-		dest.lpVtbl.pInvoke = syscall.NewCallback(dispInvoke)
-		dest.iface = disp
-		dest.iid = iid
-		cookie, err = point.Advise((*ole.IUnknown)(unsafe.Pointer(dest)))
-		container.Release()
-		if err != nil {
-			point.Release()
-			return
-		}
-	}
-
-	container.Release()
-
-	return 0, ole.NewError(ole.E_INVALIDARG)
 }
 
 func dispQueryInterface(this *ole.IUnknown, iid *ole.GUID, punk **ole.IUnknown) uint32 {
