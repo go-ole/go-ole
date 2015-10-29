d084b115f2373037c4648da1182fb5f40986aceb
Jacob Santos <jacob@essential-elements.net>
2015-10-29 14:55:13 -0500

    Split test case into separate test cases to find problems.

--- idispatch_windows_test.go
+++ idispatch_windows_test.go
@@ -2,82 +2,212 @@
 
 package ole
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func TestIDispatch(t *testing.T) {
+func wrapCOMExecute(t *testing.T, callback func(*testing.T)) {
 	defer func() {
 		if r := recover(); r != nil {
 			t.Error(r)
 		}
 	}()
 
-	var err error
-
-	err = CoInitialize(0)
+	err := CoInitialize(0)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	defer CoUninitialize()
 
+	callback(t)
+}
+
+func wrapDispatch(t *testing.T, ClassID, UnknownInterfaceID, DispatchInterfaceID *GUID, callback func(*testing.T, *IUnknown, *IDispatch)) {
 	var unknown *IUnknown
 	var dispatch *IDispatch
 
-	// oleutil.CreateObject()
-	unknown, err = CreateInstance(CLSID_COMEchoTestObject, IID_IUnknown)
+	unknown, err = CreateInstance(ClassID, UnknownInterfaceID)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 		return
 	}
 	defer unknown.Release()
 
-	dispatch, err = unknown.QueryInterface(IID_ICOMEchoTestObject)
+	dispatch, err = unknown.QueryInterface(DispatchInterfaceID)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 		return
 	}
 	defer dispatch.Release()
 
-	echoValue := func(method string, value interface{}) (interface{}, bool) {
-		var dispid []int32
-		var err error
+	callback(t, unknown, dispatch)
+}
 
-		dispid, err = dispatch.GetIDsOfName([]string{method})
+func wrapGoOLETestCOMServer(t *testing.T, callback func(*testing.T, *IUnknown, *IDispatch)) {
+	wrapCOMExecute(t, func(t *testing.T) {
+		wrapDispatch(t, CLSID_COMEchoTestObject, IID_IUnknown, IID_ICOMEchoTestObject, callback)
+	})
+}
+
+func TestIDispatch_goolecomserver_echostring(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoString"
+		expected := "Test String"
+		variant, err := idispatch.CallMethod(method, expected)
 		if err != nil {
-			t.Fatal(err)
-			return nil, false
+			t.Error(err)
 		}
+		actual := variant.Value().(string)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
 
-		result, err := dispatch.Invoke(dispid[0], DISPATCH_METHOD, value)
+func TestIDispatch_goolecomserver_echoint8(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoInt8"
+		expected := int8(1)
+		variant, err := idispatch.CallMethod(method, expected)
 		if err != nil {
-			t.Fatal(err)
-			return nil, false
+			t.Error(err)
 		}
-
-		return result.Value(), true
-	}
-
-	methods := map[string]interface{}{
-		"EchoInt8":    int8(1),
-		"EchoInt16":   int16(1),
-		"EchoInt32":   int32(1),
-		"EchoInt64":   int64(1),
-		"EchoUInt8":   uint8(1),
-		"EchoUInt16":  uint16(1),
-		"EchoUInt32":  uint(1),
-		"EchoUInt64":  uint64(1),
-		"EchoFloat32": float32(1.2),
-		"EchoFloat64": float64(1.2),
-		"EchoString":  "Test String"}
-
-	for method, expected := range methods {
-		if actual, passed := echoValue(method, expected); passed {
-			if !reflect.DeepEqual(expected, actual) {
-				t.Errorf("%s() expected %v did not match %v", method, expected, actual)
-			}
+		actual := variant.Value().(int8)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
 		}
-	}
+	})
+}
+
+func TestIDispatch_goolecomserver_echouint8(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoUInt8"
+		expected := uint8(1)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(uint8)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echoint16(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoInt16"
+		expected := int16(1)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(int16)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echouint16(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoUInt16"
+		expected := uint16(1)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(uint16)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echoint32(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoInt32"
+		expected := int32(2)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(int32)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echouint32(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoUInt32"
+		expected := uint32(4)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(uint32)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echoint64(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoInt64"
+		expected := int64(1)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(int64)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echouint64(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoUInt64"
+		expected := uint64(1)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(uint64)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echofloat32(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoFloat32"
+		expected := float32(2.2)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(float32)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
+}
+
+func TestIDispatch_goolecomserver_echofloat64(t *testing.T) {
+	wrapGoOLETestCOMServer(t, func(t *testing.T, unknown *IUnknown, idispatch *IDispatch) {
+		method := "EchoFloat64"
+		expected := float64(2.2)
+		variant, err := idispatch.CallMethod(method, expected)
+		if err != nil {
+			t.Error(err)
+		}
+		actual := variant.Value().(float64)
+		if actual != expected {
+			t.Errorf("%s() expected %v did not match %v", method, expected, value)
+		}
+	})
 }
