86ff8eb1a825a1b75f247ba8916b724154ed39b6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-03-11 10:04:40 +0900

    Fix stub implementation
    Fixes #120

--- safearray_func.go
+++ safearray_func.go
@@ -120,8 +120,8 @@ func safeArrayGetElementSize(safearray *SafeArray) (*uint32, error) {
 }
 
 // safeArrayGetElement retrieves element at given index.
-func safeArrayGetElement(safearray *SafeArray, index int64) (uintptr, error) {
-	return uintptr(0), NewError(E_NOTIMPL)
+func safeArrayGetElement(safearray *SafeArray, index int64, pv unsafe.Pointer) error {
+	return NewError(E_NOTIMPL)
 }
 
 // safeArrayGetElement retrieves element at given index and converts to string.
