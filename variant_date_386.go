23907a0f0ac13ad6a9c940552a11bf8c30fc9c41
t_max <1172915550@qq.com>
2023-10-23 18:01:25 +0800

    fix: GetVariantDate return with milliseconds

--- variant_date_386.go
+++ variant_date_386.go
@@ -1,3 +1,4 @@
+//go:build windows && 386
 // +build windows,386
 
 package ole
@@ -9,14 +10,37 @@ import (
 	"unsafe"
 )
 
+const ONETHOUSANDMILLISECONDS = 0.0000115740740740
+
 // GetVariantDate converts COM Variant Time value to Go time.Time.
 func GetVariantDate(value uint64) (time.Time, error) {
+	halfSecond := ONETHOUSANDMILLISECONDS / 2.0
+	dVariantTime := *(*float64)(unsafe.Pointer(&value))
 	var st syscall.Systemtime
-	v1 := uint32(value)
-	v2 := uint32(value >> 32)
+	adjustedVariantTime := dVariantTime - halfSecond
+	uAdjustedVariantTime := *(*uint64)(unsafe.Pointer(&adjustedVariantTime))
+	v1 := uint32(uAdjustedVariantTime)
+	v2 := uint32(uAdjustedVariantTime >> 32)
 	r, _, _ := procVariantTimeToSystemTime.Call(uintptr(v1), uintptr(v2), uintptr(unsafe.Pointer(&st)))
 	if r != 0 {
-		return time.Date(int(st.Year), time.Month(st.Month), int(st.Day), int(st.Hour), int(st.Minute), int(st.Second), int(st.Milliseconds/1000), time.UTC), nil
+		fraction := dVariantTime - float64(int(dVariantTime))
+		hours := (fraction - float64(int(fraction))) * 24
+		minutes := (hours - float64(int(hours))) * 60
+		seconds := (minutes - float64(int(minutes))) * 60
+		milliseconds := (seconds - float64(int(seconds))) * 1000
+		milliseconds = milliseconds + 0.5
+		if milliseconds < 1.0 || milliseconds > 999.0 {
+			var st2 syscall.Systemtime
+			v1 = uint32(value)
+			v2 = uint32(value >> 32)
+			r2, _, _ := procVariantTimeToSystemTime.Call(uintptr(v1), uintptr(v2), uintptr(unsafe.Pointer(&st2)))
+			if r2 != 0 {
+				return time.Date(int(st2.Year), time.Month(st2.Month), int(st2.Day), int(st2.Hour), int(st2.Minute), int(st2.Second), 0, time.UTC), nil
+			} else {
+				return time.Now(), errors.New("Could not convert to time, passing current time.")
+			}
+		}
+		return time.Date(int(st.Year), time.Month(st.Month), int(st.Day), int(st.Hour), int(st.Minute), int(st.Second), int(int16(milliseconds))*1e6, time.UTC), nil
 	}
 	return time.Now(), errors.New("Could not convert to time, passing current time.")
 }
