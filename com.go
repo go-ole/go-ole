1a0edce883337f4571e830589a4af217176ac59d
Jacob Santos <jacobsantos@jacobsantos.com>
2025-03-06 09:38:07 -0600

    fix: Pass reference to CoGetObject
    Just pass object.

--- com.go
+++ com.go
@@ -224,7 +224,7 @@ func GetObject[T IsIUnknown](programID string, bindOpts *windows.BIND_OPTS3, int
 		windows.StringToUTF16Ptr(programID),
 		bindOpts,
 		&interfaceId,
-		&unk)
+		unk)
 	if hr == nil {
 		return
 	}
