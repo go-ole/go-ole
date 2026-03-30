1cc1dc51e3b12358b56160358c511b1858496d25
Jacob Santos <jacobsantos@jacobsantos.com>
2026-03-29 22:12:51 -0500

    build: streamline workflows and switch to regsvr32 for assembly registration
    - Combined `download` and `build` jobs into a single streamlined `build` step.
    - Updated to use `.NET 9` setup.
    - Replaced `RegAsm` with `regsvr32` for assembly registration.
    - Explicitly specified version `v2.0.0` for test COM server downloads.

--- build/register-assembly.bat
+++ build/register-assembly.bat
@@ -1,8 +1,3 @@
 @ECHO OFF
 
-IF "x86" == "%DOWNLOADPLATFORM%" (
-	CALL c:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /codebase /nologo c:\gopath\src\github.com\go-ole\go-ole\TestCOMServer.dll
-)
-IF "x64" == "%DOWNLOADPLATFORM%" (
-	CALL c:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /codebase /nologo c:\gopath\src\github.com\go-ole\go-ole\TestCOMServer.dll
-)
+regsvr32 /s c:\gopath\src\github.com\go-ole\go-ole\TestCOMServer.comhost.dll
